722726265ba81cffabf9455467d013bf44f02b28
BelyaevAlex <77803344+BelyaevAlex@users.noreply.github.com>
2021-07-10 14:50:04 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -124,7 +124,6 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
             save_path = str(save_dir / p.name)  # img.jpg
             txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
             s += '%gx%g ' % img.shape[2:]  # print string
-            print(img.shape[2:])
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             imc = im0.copy() if save_crop else im0  # for save_crop
             if len(det):
@@ -139,6 +138,7 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                 print(names[int(c)], ' ')
                 print(s, ' ')
                 print(n, ' ')
+                print(im0.shape)
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
