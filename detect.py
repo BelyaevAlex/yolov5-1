7764b347e715d289d560e6feb59b79bf2c08f1d0
BelyaevAlex <77803344+BelyaevAlex@users.noreply.github.com>
2021-07-10 22:50:00 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -154,13 +154,13 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                 fi = 0
                 u = 0
                 for i in range(5):
-                    for j in range(len(truedet)):
+                    for j in range(len(truedet)-u):
                         if truedet[j].isdigit():
                             numb[i] = truedet[j:j+2]
                             break
                     u = truedet.find(',')
                     truedet = truedet[truedet.find(','):] 
-                a[tr] = [p.name, '%gx%g ' % img.shape[2:], numb[:4], numb[5]]
+                a[tr] = [p.name, '%gx%g ' % img.shape[2:], numb[:3], numb[4]]
                 tr += 1
                     
 
