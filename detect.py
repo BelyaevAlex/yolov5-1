c4acb0f4b801bd6a932b9062d48a50cfa0426c4a
BelyaevAlex <77803344+BelyaevAlex@users.noreply.github.com>
2021-07-10 14:40:35 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -142,7 +142,8 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
-                        print("\n"conf)
+                        print("\n")
+                        print(conf)
 
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
