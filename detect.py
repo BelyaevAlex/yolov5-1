68f752d10de23c8c5e7d3d48545505f64814a463
BelyaevAlex <77803344+BelyaevAlex@users.noreply.github.com>
2021-07-10 14:42:17 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -140,10 +140,11 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                        with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
                         print("\n")
                         print(conf)
+                        with open(txt_path + '.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                        
 
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
