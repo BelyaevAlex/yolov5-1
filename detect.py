cad96bf3f4b548fa1603a099b20302932d37536c
BelyaevAlex <77803344+BelyaevAlex@users.noreply.github.com>
2021-07-11 12:44:18 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -209,6 +209,7 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
     fa = a
     numb = [0] * 5
     l = 0
+    score = 0
     for i in range(ds):
         fa[i][2] = str(fa[i][2])[7:]
         for j in range(fa[i][2].count(']')-1):
@@ -242,7 +243,7 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                 u = a[i][2].find(',')
                 a[i][2] = a[i][2][a[i][2].find(',')+1:]
             a[i][2] = a[i][2][a[i][2].find(']')+2:]
-            df[sscore] = [a[i][0], a[i][0], numb[0], numb[1], numb[2], numb[3], numb[4]]
+            df[sscore] = [a[i][0], a[i][1], numb[0], numb[1], numb[2], numb[3], numb[4]]
             sscore += 1
             
     df = pd.Series(df)
