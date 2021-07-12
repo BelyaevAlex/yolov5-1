b6613afa4c6413aec91d5d2b231bef437e3f9432
BelyaevAlex <77803344+BelyaevAlex@users.noreply.github.com>
2021-07-12 18:05:52 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -193,7 +193,7 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                         vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                     vid_writer[i].write(im0)
     
-    df = pd.DataFrame(data=df, columns=['img_name', 'img_weight', 'img_height', 'x1', 'y1', 'x2', 'y2', 'confidence', 'class', 'name_class'])
+    df = pd.DataFrame(data=df, columns=['img_name', 'img_height', 'img_width', 'x1', 'y1', 'x2', 'y2', 'confidence', 'class', 'name_class'])
     df.to_csv('out.csv', index=False)
 
     if save_txt or save_img:
