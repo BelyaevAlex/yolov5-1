ebf5ab9bbb49a82d12f6adb5a58e45fc1147b661
BelyaevAlex <77803344+BelyaevAlex@users.noreply.github.com>
2021-07-13 19:26:34 +0300

    Update test.py

--- test.py
+++ test.py
@@ -142,14 +142,15 @@ def run(data,
         t2 += time_synchronized() - t
         numb = 0
         for i in range(len(out)):
-            trush = 0
-            number = 0
-            for x1, y1, x2, y2, s, *b in out[i]:
-               if x2 - x1 <= N:
-                    trush = out[numb].tolist().pop(number)
-                    number -= 1
-               number += 1
-               numb += 1
+            for pred in out[i]:
+                trush = 0
+                number = 0
+                for x1, y1, x2, y2, s, *b in pred.numpy():
+                    if x2 - x1 <= N:
+                         trush = out[numb].tolist().pop(number)
+                         number -= 1
+                    number += 1
+                    numb += 1
         # Statistics per image
         for si, pred in enumerate(out):
             labels = targets[targets[:, 0] == si, 1:]
