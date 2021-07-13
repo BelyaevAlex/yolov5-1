6a7ffeedca67849b044fe0ce05d3721d262476c8
BelyaevAlex <77803344+BelyaevAlex@users.noreply.github.com>
2021-07-13 18:58:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -141,15 +141,16 @@ def run(data,
         out = non_max_suppression(out, conf_thres, iou_thres, labels=lb, multi_label=True, agnostic=single_cls)
         t2 += time_synchronized() - t
         numb = 0
-        for pred in out:
-            trush = 0
-            number = 0
-            for x1, y1, x2, y2, s, b in pred.numpy():
-               if x2 - x1 <= N:
-                    trush = out[numb].tolist().pop(number)
-                    number -= 1
-               number += 1
-            numb += 1
+        for i in range(len(out)):
+            for pred in out[i]:
+                trush = 0
+                number = 0
+                for x1, y1, x2, y2, s, b in pred.numpy():
+                   if x2 - x1 <= N:
+                        trush = out[numb].tolist().pop(number)
+                        number -= 1
+                   number += 1
+                   numb += 1
         # Statistics per image
         for si, pred in enumerate(out):
             labels = targets[targets[:, 0] == si, 1:]
