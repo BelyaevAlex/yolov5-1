e6515007f0d34bd419f144735c7ebd5708e7add2
BelyaevAlex <77803344+BelyaevAlex@users.noreply.github.com>
2021-07-15 16:24:51 +0300

    Update test.py

--- test.py
+++ test.py
@@ -125,7 +125,7 @@ def run(data,
         
         
         
-        targets = targets[targets[:, 2] - targets[:, 4]  > box_width_thres/10000]
+        targets = targets[targets[:, 2] - targets[:, 4]  > box_width_thres/100]
         for i in targets:
             df.append(i)
         targets = targets.to(device)
