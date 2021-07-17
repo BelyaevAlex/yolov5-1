af633640e477b252e7fbb5fcf988b6c7730b5ddd
BelyaevAlex <77803344+BelyaevAlex@users.noreply.github.com>
2021-07-17 13:08:07 +0300

    Update test.py

--- test.py
+++ test.py
@@ -125,7 +125,7 @@ def run(data,
         
         
         df.append(paths)
-        targets = targets[targets[:, 2] * width - targets[:, 4] * height  > box_width_thres]
+        targets = targets[targets[:, 2] * img.shape[3] - targets[:, 4] * img.shape[2]  > box_width_thres]
         targets = targets.to(device)
         nb, _, height, width = img.shape  # batch size, channels, height, width
         t = time_synchronized()
